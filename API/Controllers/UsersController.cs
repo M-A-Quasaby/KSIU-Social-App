89841e74eb76b06d1aa37f93fbf80fd9d9228b5c
Mohamed Ahmed El Quasaby <67268725+M-A-Quasaby@users.noreply.github.com>
2023-06-16 20:00:45 +0300

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly KSIUContext _context;
+
+        public UsersController(KSIUContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetUsers() 
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ API/Data/KSIUContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class KSIUContext : DbContext
+    {
+        public KSIUContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
